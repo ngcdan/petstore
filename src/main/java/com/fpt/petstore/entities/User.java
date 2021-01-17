a9b20d39f6b6eff91d68a4b98f0b05040ec9f2c6
LeNgocDan <Linuss1908@gmail.com>
2021-01-17 15:36:31 +0700

    fix user entity

--- src/main/java/com/fpt/petstore/entities/User.java
+++ src/main/java/com/fpt/petstore/entities/User.java
@@ -1,12 +1,25 @@
 package com.fpt.petstore.entities;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fpt.petstore.security.Authority;
+import com.fpt.petstore.security.UserRole;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +33,9 @@ uniqueConstraints = {
 @JsonInclude(Include.NON_NULL)
 @Setter @Getter
 @NoArgsConstructor
-public class User extends AbstractPersistable<Long> {
+public class User extends AbstractPersistable<Long> implements UserDetails {
+
+  private static final long serialVersionUID = 1L;
   
   private String username;
   private String password;
@@ -33,5 +48,30 @@ public class User extends AbstractPersistable<Long> {
   private String phone;
   
   private boolean enabled=true;
+  
+  @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @JsonIgnore
+  private Set<UserRole> userRoles = new HashSet<>();
 
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+      Set<GrantedAuthority> authorities = new HashSet<>();
+      userRoles.forEach(ur -> authorities.add(new Authority(ur.getRole().getName())));
+      return authorities;
+  }
+  
+  @Override
+  public boolean isAccountNonExpired() {
+      return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+      return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+      return true;
+  }
 }
