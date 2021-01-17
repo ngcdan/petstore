b918880cbbf255b5aefd1993f6d1a18a8b31e15e
LeNgocDan <Linuss1908@gmail.com>
2021-01-17 09:46:43 +0700

    initial project

---
+++ src/main/java/com/fpt/petstore/PetStoreApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/petstore/PetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
