ee4d5c62bd94d1d3aed340cb5770318886044100
LeNgocDan <Linuss1908@gmail.com>
2021-01-17 21:22:40 +0700

    fix code

---
+++ src/main/web/webapp/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './common/Header';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
