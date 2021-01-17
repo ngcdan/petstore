f9074b99e5b74a275050a8cb3a57b272f9f106f2
LeNgocDan <Linuss1908@gmail.com>
2021-01-17 15:56:11 +0700

    fix code

--- src/main/java/com/fpt/petstore/controller/HomeController.java
+++ src/main/java/com/fpt/petstore/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.fpt.petstore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +10,17 @@ public class HomeController {
   @RequestMapping("/")
   public String index() {
     return "index";
+  }
+  
+  @RequestMapping("/myAccount")
+  public String myAccount() {
+    return "myAccount";
+  }
+  
+  @RequestMapping("/login")
+  public String login(Model model ) {
+    model.addAttribute("classActiveLogin", true);
+    return "myAccount";
   }
 
 }
