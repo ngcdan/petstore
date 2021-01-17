bcb74c74f217c95a4003d7b260d69fbdc9ddfed3
LeNgocDan <Linuss1908@gmail.com>
2021-01-17 21:25:38 +0700

    add header

--- src/main/web/webapp/src/common/Header.tsx
+++ src/main/web/webapp/src/common/Header.tsx
@@ -1,18 +1,5 @@
 import React, { useState } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText } from 'reactstrap';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
