9dfcb805c7b268b562ec71dfda5727a0ef136c47
LeNgocDan <Linuss1908@gmail.com>
2021-01-17 15:32:28 +0700

    add role

--- src/main/java/com/fpt/petstore/security/Role.java
+++ src/main/java/com/fpt/petstore/security/Role.java
@@ -11,15 +11,22 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-import com.adminportal.domain.security.UserRole;
 import com.fpt.petstore.entities.AbstractPersistable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * @author linuss
  */
 
 @Entity
+@Table(name = "role")
+@Setter @Getter
+@NoArgsConstructor
 public class Role extends AbstractPersistable<Serializable> {
   
   private String name;
@@ -27,4 +34,8 @@ public class Role extends AbstractPersistable<Serializable> {
   @OneToMany(mappedBy = "role" ,cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   private Set<UserRole> userRoles = new HashSet<>();
 
+  public Role withUserRoles(UserRole userRole ) {
+    this.userRoles.add(userRole);
+    return this;
+  }
 }
