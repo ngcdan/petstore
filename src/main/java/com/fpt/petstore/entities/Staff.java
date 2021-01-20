3374ffc7944c86c1214b5ad7708326b046087fde
LeNgocDan <Linuss1908@gmail.com>
2021-01-20 16:19:43 +0700

    fix entity

--- src/main/java/com/fpt/petstore/entities/Staff.java
+++ src/main/java/com/fpt/petstore/entities/Staff.java
@@ -12,7 +12,6 @@ import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fpt.petstore.entities.User.UserRole;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,6 +30,7 @@ uniqueConstraints = {
 @Setter @Getter
 @NoArgsConstructor
 public class Staff extends AbstractPersistable<Long> {
+  static public enum UserRole { Admin, Staff };
 
   private String username;
   private String password;
@@ -44,13 +44,39 @@ public class Staff extends AbstractPersistable<Long> {
 
   private String avatar;
 
-  private boolean gender;
+  private boolean gender = true;
 
   private String address;
 
   private boolean enabled=true;
 
   @Enumerated(EnumType.STRING)
-  private UserRole role = UserRole.Admin;
+  private UserRole role = UserRole.Staff;
+
+  public Staff(String username, String email, String phone) {
+    this.username = username;
+    this.email    = email;
+    this.phone    = phone;
+  }
+  
+  public Staff withFirstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public Staff withLastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  public Staff withAddress(String address) {
+    this.address = address;
+    return this;
+  }
+
+  public Staff withPassword(String password) {
+    this.password = password;
+    return this;
+  }
 
 }
--- src/main/java/com/fpt/petstore/entities/User.java
+++ src/main/java/com/fpt/petstore/entities/User.java
@@ -4,8 +4,6 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -21,26 +19,20 @@ import lombok.Setter;
 @Entity
 @Table(name = "user",
 uniqueConstraints = {
-    @UniqueConstraint(columnNames = {"id", "username", "email"})
+    @UniqueConstraint(columnNames = {"id", "phone", "email"})
 })
 @JsonInclude(Include.NON_NULL)
 @Setter @Getter
 @NoArgsConstructor
 public class User extends AbstractPersistable<Long> {
 
-  static public enum UserRole { Admin, Staff, User };
-
-  private String username;
-  private String password;
-  private String firstName;
-  private String lastName;
-
   @Column(name = "email", nullable = false,updatable = false)
   private String email;
 
   private String phone;
-
-  private String avatar;
+  private String password;
+  private String firstName;
+  private String lastName;
 
   @JsonFormat(pattern = DateUtil.LOCAL_DATETIME_FORMAT)
   private Date dateOfBirth = new Date();
@@ -49,33 +41,28 @@ public class User extends AbstractPersistable<Long> {
 
   private String address;
 
-  private boolean enabled=true;
-
-  @Enumerated(EnumType.STRING)
-  private UserRole role = UserRole.Admin;
-
-  public User(String username, String email) {
-    this.username = username;
+  public User(String phone, String email) {
+    this.phone = phone;
     this.email = email;
   }
-  
+
   public User withFirstName(String firstName) {
     this.firstName = firstName;
     return this;
   }
-  
+
   public User withLastName(String lastName) {
     this.lastName = lastName;
     return this;
   }
-  
-  public User withPhone(String phone) {
-    this.phone = phone;
-    return this;
-  }
-  
+
   public User withAddress(String address) {
     this.address = address;
+    return this;
+  }
+
+  public User withPassword(String password) {
+    this.password = password;
     return this;
   }
 
