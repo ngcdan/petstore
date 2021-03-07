b7d29b6de31552f5f23c9a5a490b0c1499719567
LeNgocDan <Linuss1908@gmail.com>
2021-03-07 22:32:06 +0700

    update code

--- src/main/java/com/fpt/petstore/controller/AuthController.java
+++ src/main/java/com/fpt/petstore/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.fpt.petstore.controller;
 
-import com.fpt.petstore.request.CustomerRegisterRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
