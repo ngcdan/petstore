997681eeab0b2e3a75049b45c65ff9238cd6e756
LeNgocDan <Linuss1908@gmail.com>
2021-03-07 23:04:55 +0700

    update code

--- src/main/java/com/fpt/petstore/config/H2SecurityConfig.java
+++ src/main/java/com/fpt/petstore/config/H2SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.fpt.petstore.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@Configuration
-//@Order(1)
+@Configuration
 public class H2SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.
