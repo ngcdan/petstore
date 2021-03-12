88df90f2d291379d649452678ba2589c0741d39c
LeNgocDan <Linuss1908@gmail.com>
2021-03-12 16:27:09 +0700

    code review

--- src/main/java/com/fpt/petstore/config/H2SecurityConfig.java
+++ src/main/java/com/fpt/petstore/config/H2SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class H2SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,8 +21,21 @@ public class H2SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.
-			authorizeRequests().antMatchers("/console/**").permitAll();
+			authorizeRequests().antMatchers("/**").permitAll();
 		httpSecurity.csrf().disable();
+		httpSecurity.cors();
 		httpSecurity.headers().frameOptions().disable();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
